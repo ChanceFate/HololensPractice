1631f478836ad20815c8ebb576ad85b7ed8644d9
Nick <nick.borchers@gmail.com>
2016-07-31 20:21:55 -0400

    Added Underworld to app, accessible via the HitTarget.cs script.

---
+++ Scripts/HitTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HitTarget : MonoBehaviour
+{
+
+    public GameObject underworld;
+    public GameObject objectToHide;
+
+	// Occurs when this object starts colliding with another
+    void OnCollisionEnter(Collision collision)
+    {
+        // Hide the stage and show the underworld
+        objectToHide.SetActive(false);
+        underworld.SetActive(true);
+
+        // Disable Spatial Mapping to allow the spheres to enter the underworld.
+        SpatialMapping.Instance.SetMappingEnabled(false);
+    }
+}
